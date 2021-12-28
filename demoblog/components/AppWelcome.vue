9b5212b98c45449ad8f5423d03f3e6df83146c2d
sanjaysanjeldh <ssanjel@deerhold.org>
2021-12-28 18:22:44 +0545

    image verified

--- demoblog/components/AppWelcome.vue
+++ demoblog/components/AppWelcome.vue
@@ -3,34 +3,19 @@
     <div class="max-w-lg mx-auto">
       <div class="flex justify-center mb-6">
         <div>
-          <img
-            src="/welcome/content-light.svg"
-            alt="welcome image"
-            height="30"
-            class="light-img w-full"
-          />
-          <img
-            src="/welcome/content-dark.svg"
-            alt="welcome image"
-            class="dark-img w-full"
-          />
+          <div
+            class="
+              xl:text-lg
+              font-medium
+              leading-relaxed
+              text-center
+              flex
+              justify-center
+            "
+          >
+            <img src="../static/icon.png" class="w-32 h-auto" />
+          </div>
         </div>
-        <div>
-          <img
-            src="/welcome/nuxt-light.svg"
-            alt="welcome image"
-            class="light-img w-full"
-          />
-          <img
-            src="/welcome/nuxt-dark.svg"
-            alt="welcome image"
-            class="dark-img w-full"
-          />
-        </div>
-      </div>
-      <div class="xl:text-lg font-medium leading-relaxed text-center flex justify-center ">
-        <img src="../static/icon.png" class="w-32 h-auto"/>
-        
       </div>
     </div>
   </div>
